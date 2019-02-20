53421c9665ea2c17df333d7beb1abcde286cee55
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-02-20 12:50:24 +0000

    Binary file example updated

--- file.cpp
+++ file.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 
 int main() {
+    // Text file example
     std::string data;
     std::fstream file;
     file.open("test.txt", std::ios::in);
@@ -11,11 +12,20 @@ int main() {
     }
     file.close();
 
-    int x = 0xFFFFFFFF;
+    // Binary file example
+    int x = 128;
+    char buffer[sizeof(int)];
+    int int_buffer;
+
     std::fstream binary_file;
     binary_file.open("text.bin", std::ios::binary | std::ios::in | std::ios::out);
-    binary_file.write((char*)&x, 8);
+    binary_file.write((char*)&x, sizeof(int));
     binary_file.close();
-    
+
+    binary_file.open("text.bin", std::ios::binary | std::ios::in | std::ios::out);
+    binary_file.read((char*)&int_buffer, sizeof(int));
+    std::cout << int_buffer << "\n" << x << "\n";
+    binary_file.close();
+
     return 0;
 }
